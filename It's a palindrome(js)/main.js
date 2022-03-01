9eaff0940aacf2d568788e81710f408741a211af
rexx0n <alexey.mironenko46@gmail.com>
2022-03-01 16:43:23 +0300

    some edits

--- It's a palindrome(js)/main.js
+++ It's a palindrome(js)/main.js
@@ -1,33 +1,27 @@
 //first variation
 function isPoli(arg) {
-    if (arg === "") {
+  if (arg === "") {
+    return false
+  }
+  let a = arg.split("").reverse().join("")
+  return a === arg
+}
+isPoli('')
+console.log(isPoli('ДОВОД') === true);
+console.log(isPoli('cat') === false);
+console.log(isPoli('AABBAA') === true);
+
+// second variation
+
+function isPolig(text) {
+  let c = Math.floor(text.length / 2)
+  for (let i = 0; i < c; i++) {
+    if (text[i] !== text[text.length - i - 1]) {
       return false
     }
-    let a = arg.split("").reverse().join("") 
-    return a === arg
   }
-  isPoli('')
-  console.log(isPoli('ДОВОД')===true);
-  console.log(isPoli('cat')===false);
-  console.log(isPoli('AABBAA')===true);
-
-  // second variation
-
-  function isPolig(text) {
-    let c = Math.floor(text.length / 2) 
-    for (let i = 0; i >=c;) {
-        if (i>=c) {
-            return true
-        }
-        else {
-          if(text[i] === text[text.length - i + 1]) {
-            i = i + 1
-          }
-          else{
-            return false
-          }
-      
-        }
-    }
-  }
-  console.log(isPolig('ДОВОД')===true)
+  return true
+}
+console.log(isPolig('ДОВОД') === true)
+console.log(isPoli('cat') === false);
+console.log(isPoli('AABBAA') === true)
