b7f544fa7542d7ce80cafb728a736984b1a1d8ab
rexx0n <alexey.mironenko46@gmail.com>
2022-03-21 20:46:31 +0300

    edits

--- Wordle/main.js
+++ Wordle/main.js
@@ -6,7 +6,8 @@ let wordle = {
     //если больше 5 то отмена
     //если меньше 5 то отмена
     if (this.isValid(str) === false) {
-      return;
+                                                            //если меньше 5 символов то вывести что нужно 5
+      return ;
     }
     //если 5 то ввести по символу в ячейки
     let row = document.getElementById(this.try).querySelectorAll("span");
@@ -23,13 +24,13 @@ let wordle = {
           row[i].classList.add("green");
         }
       }
-
+                                                                        //будет отлично если сделать ниже алфавит и вычеркивать буквы которые были использованы
       // если буква стоит в правильном место то покрасить в зеленый
     }
     //проверить совпадают ли слова
     // если да то вывести победа если нет то увеличить попытку
     if (this.isWin(str) === true) {
-      alert("You Win");
+                                                                          //алерт заменить на вывод в html 
     } else {
       this.try++;
       //если попыток не осталось то вывести проигрыш, макс 6 попыток
@@ -41,15 +42,24 @@ let wordle = {
   try: 0,
   // функция возвращает true если simfol есть в загаданом слове (this.word) и не стоит на своей позиции
   isYellow(simfol, index) {
-    throw new Error("Не реализована");
-    
+    //throw new Error("Не реализована");
+    if (isCharExist(simfol, this.word) != null && isCharExist(simfol, this.word) !== index) {
+      return true
+    } 
+    else {
+      return false
+    }
   },
   // функция возвращает true если simfol есть в загаданом слове (this.word) и стоит на своей позиции
   isGreen(simfol1, index1) {
-    throw new Error("Не реализована");
+    if (isCharExist(simfol1, this.word) !==null && isCharExist(simfol1, this.word) === index1) {
+      return true
+    }
+    else {
+      return false
+    }
   },
   getCurrent() {
-    // throw new Error("Не реализована")
     let input = document.getElementById("input");
     return input.value;
   },
@@ -58,10 +68,15 @@ let wordle = {
     return str.length === 5;
   },
   isWin(text) {
-    throw new Error("Не реализована");
+    //throw new Error("Не реализована");  
+    if (text === this.word) {
+      return true
+    }
+    else {
+      return false
+    }
   },
   setRandomWord() {
-    // throw new Error("Не реализована");
     let i = Math.random() * (this.dict.length - 1);
     i = Math.round(i);
     this.word = this.dict[i];
@@ -75,5 +90,13 @@ let wordle = {
   dict: ["ложка", "рюмка"],
   word: null,
 };
-
+//функция проверяет если ли символ в слове и если есть возвращает номер его позиции в слове, а если нет возвращает null
+function isCharExist (symbol, word) {
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === symbol) {
+        return i
+      }
+  } 
+  return null
+}
 wordle.init()
