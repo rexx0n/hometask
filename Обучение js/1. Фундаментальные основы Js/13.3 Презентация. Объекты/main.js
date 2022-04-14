92363cb079186869a714e43b4524ba9eee44b9ca
rexx0n <alexey.mironenko46@gmail.com>
2022-04-14 19:16:16 +0300

    completed three assignments on "objects"

---
+++ Обучение js/1. Фундаментальные основы Js/13.3 Презентация. Объекты/main.js
@@ -0,0 +1,15 @@
+// 1. Создать объект с полем product, равным ‘iphone’
+
+const company = {
+  product: "iphone",
+};
+
+// 2. Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+
+company.price = 1000;
+company.currency = "dollar";
+
+// 3. Добавить поле details, которое будет содержать объект с полями model и color
+
+company.details = { model: "sda", color: "blue" };
+console.log(company)
