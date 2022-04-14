e736597d7a7121008154dc7429fc6aae690d4a1a
rexx0n <alexey.mironenko46@gmail.com>
2022-04-14 20:28:33 +0300

    solved 5 problems on "if, else"

---
+++ Обучение js/1. Фундаментальные основы Js/15.2 Презентация. Операторы и конструкция if else/main.js
@@ -0,0 +1,65 @@
+// 1. Если переменная равна “hidden”, присвоить ей значение “visible”, иначе - “hidden”.
+
+let a = "hidden";
+if (a === "hidden") {
+  a = "visible";
+} else {
+  a = "hidden";
+}
+console.log(a);
+
+// 2.Используя if, записать условие:
+//  если переменная равна нулю, присвоить ей 1;
+// если меньше нуля - строку “less then zero”;
+// если больше нуля - используя оператор “присвоение”, переменную умножить на 10 (использовать краткую запись).
+
+let b = -1;
+if (b === 0) {
+  b = 1;
+} else if (b < 0) {
+  b = "less them zero";
+} else if (b > 0) {
+  b += b * 10;
+}
+console.log(b);
+
+// 3. Дан объект let car = { name: 'Lexus', age: 10, create: 2008, needRepair: false }.
+// Написать условие если возраст машины больше 5 лет то нужно вывести в консоль сообщение 'Need Repair' и свойство needRepair в объекте car изменить на true; иначе изменить на false.
+let car = { name: "Lexus", age: 10, create: 2008, needRepair: false };
+if (car.age > 5) {
+  console.log("Need Repair");
+  car.needRepair = true;
+}
+console.log(car);
+
+// 4. Дан объект let item = { name: 'Intel core i7', price: '100$', discount: '15%' }.
+// Написать условие если у item есть поле discount и там есть значение которое не NaN а также есть поле price значение которого также не NaN то в объекте item создать поле priceWithDiscount и записать туда цену с учетом скидки и вывести ее в консоль, обратите внимание что поля discount и price это строки и вам из них нужно получить числа чтобы выполнить расчет. иначе если поля discount нет то вывести просто поле price в консоль.
+
+let item = { name: "Intel core i7", price: "100", discount: "15" };
+if (item.discount !== NaN && item.price !== NaN) {
+  function discount(price, discount) {
+    let priceNumber = Number(price);//я убрал символ доллора в обьекты чтобы сделать из строки число
+    let discountNumber = Number(discount);// и процент убрал
+    let sum = (priceNumber / 100) * discountNumber;
+
+    return priceNumber - sum;
+  }
+  item.priceWithDiscount = discount(item.price, item.discount);
+}
+console.log(item);
+
+// 5.Написать условие если цена товара больше или равна минимальной цене и меньше или равна максимальной цене то вывести в консоль название этого товара, иначе вывести в консоль что товаров не найдено.
+
+let product = {
+    name: "Яблоко",
+    price: "10"
+};
+
+let min = 10; // минимальная цена
+let max = 20; // максимальная цена
+if (product.price >= min) {
+  console.log(product.name)
+}
+else {
+  console.log("Товаров не найдено")
+}
