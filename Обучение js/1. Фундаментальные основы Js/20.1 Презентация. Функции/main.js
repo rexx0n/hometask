ac7508d944075104c4529be1dc2de8ed0f7e887a
rexx0n <alexey.mironenko46@gmail.com>
2022-04-22 18:05:41 +0300

    complete 2 tasks

--- Обучение js/1. Фундаментальные основы Js/20.1 Презентация. Функции/main.js
+++ Обучение js/1. Фундаментальные основы Js/20.1 Презентация. Функции/main.js
@@ -6,4 +6,28 @@ function multiply() {
   }
   return sum;
 }
-console.log(multiply(2,10))
+console.log(multiply(2, 10)=== 20);
+
+// Создать функцию, которая принимает строку и возвращает строку-перевертыш: reverseString(‘test’) // “tset”.
+
+function reverseString(text) {
+  let res = "";
+  for (let i = text.length - 1; i >= 0; i--) {
+    res += text[i];
+  }
+  return res;
+}
+console.log(reverseString("text") === "txet");
+
+/*Создать функцию, которая в качестве аргумента принимает строку из букв и возвращает строку, где каждый символ разделен пробелом и заменен на юникод-значение символа: 
+getCodeStringFromText(‘hello’) // “104 101 108 108 111” 
+подсказка: для получения кода используйте специальный метод
+*/
+function getCodeStringFromText(test) {
+  let result = "";
+  for (let i = 0; i < test.length; i++) {
+    result += test.charCodeAt(i) + " ";
+  }
+  return result;
+}
+console.log(getCodeStringFromText("hello") === "104 101 108 108 111 ");
