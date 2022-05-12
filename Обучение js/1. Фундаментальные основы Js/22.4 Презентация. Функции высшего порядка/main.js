48243b2b93f9585572ff86474346c8d535454c95
rexx0n <alexey.mironenko46@gmail.com>
2022-05-12 22:13:33 +0300

    update

--- Обучение js/1. Фундаментальные основы Js/22.4 Презентация. Функции высшего порядка/main.js
+++ Обучение js/1. Фундаментальные основы Js/22.4 Презентация. Функции высшего порядка/main.js
@@ -18,19 +18,66 @@ function firstFunc(arr, callback) {
   let res = "New value: ";
   for (let i = 0; i < arr.length; i++) {
     let item = arr[i];
-    let s1 =  callback(item)
+    let s1 = callback(item);
     res += s1;
   }
   return res;
 }
 function handler1(item) {
-  return  item[0].toUpperCase() + item.slice(1, item.length);
+  return item[0].toUpperCase() + item.slice(1, item.length);
 }
-function handler2 (item) {
-    return (item * 10) + ", "
+function handler2(item) {
+  return item * 10 + ", ";
+}
+function handler3(item) {
+  return;
 }
 console.log(
   firstFunc(["my", "name", "is", "Trinity"], handler1) ===
     "New value: MyNameIsTrinity"
 );
-console.log(firstFunc([10, 20, 30], handler2) === "New value: 100, 200, 300, " )
+console.log(firstFunc([10, 20, 30], handler2) === "New value: 100, 200, 300, ");
+
+let a = [1, 2, 3, 4];
+a.forEach((element) => {
+  console.log(element);
+});
+
+b = a.map((element) => {
+  return element * 10;
+});
+console.log(b);
+
+let o = [
+  { key: "IS_PRINT", value: true },
+  { key: "VIEW", value: true },
+  { key: "ADMIN", value: true },
+];
+
+
+let option = o.reduce((acc, element) => {
+  console.log(acc,element)
+  let e = element.key;
+  acc[e] = element.value;
+  return acc
+}, {});
+
+c = a.reduce((a, e) => {
+  a.push(e * 10);
+  return  a
+}, []);
+console.log(c);
+/*2. Написать аналог метода every. Создайте функцию every, она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив) вторым аргументом callback (обязательно проверьте что передана функция)
+функция должна возвращать true или false в зависимости от результата вызова callback (проверить число больше 5). Callback  должен принимать один элемент массива, его индекс в массиве и весь массив. 
+ 
+Что такое метод every можно прочитать здесь и ниже в презентации тоже о них идет речь.
+*/
+function every (arr, callback) {
+
+}
+console.log(every([1,7,5,8], (el,index, arr) => {
+  if (el >=5) {
+    return true
+  }
+  return false
+}))
