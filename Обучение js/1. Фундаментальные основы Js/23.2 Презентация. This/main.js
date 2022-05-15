2780b6e319735e69fbb9e029a3c3031e7ff44e09
rexx0n <alexey.mironenko46@gmail.com>
2022-05-15 14:33:19 +0300

    complete task

--- Обучение js/1. Фундаментальные основы Js/23.2 Презентация. This/main.js
+++ Обучение js/1. Фундаментальные основы Js/23.2 Презентация. This/main.js
@@ -2,8 +2,31 @@
 // прямоугольника, а также может посчитать площадь фигуры:
 // const rectangle = {width:..., height:..., getSquare:...};
 const rectangle = {
-    width: 20,
-    height:20,
-    getSquare: function () {return this.width * this.height}
-}
-console.log(rectangle.getSquare())
+  width: 20,
+  height: 20,
+  getSquare: function () {
+    return this.width * this.height;
+  },
+};
+console.log(rectangle.getSquare());
+// Создать объект, у которого будет цена товара и его скидка, а также
+// два метода: для получения цены и для расчета цены с учетом скидки:
+// const price = {
+//     price: 10,
+//     discount: '15%',
+// ... };
+// price.getPrice(); // 10
+// price.getPriceWithDiscount(); // 8.5
+const price = {
+  price: 10,
+  discount: "15%",
+  getPriceWithDiscount: function () {
+    let sum = (this.price / 100) * parseFloat(this.discount);
+    return this.price - sum;
+  },
+  getPrice: function () {
+    return this.price;
+  },
+};
+console.log(price.getPrice());
+console.log(price.getPriceWithDiscount());
