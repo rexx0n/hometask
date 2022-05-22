7bb00acccb77967d0f8915999ef45660f89ad0d4
rexx0n <alexey.mironenko46@gmail.com>
2022-05-22 20:52:15 +0300

    complete task

--- Обучение js/1. Фундаментальные основы Js/25.1 Презентация. Перебирающие методы массивов/main.js
+++ Обучение js/1. Фундаментальные основы Js/25.1 Презентация. Перебирающие методы массивов/main.js
@@ -2,5 +2,5 @@
 // каждый элемент которого будет хранить информацию о числе и его четности:
 // [{digit: 1, odd: true}, {digit: 2, odd: false}, {digit: 3, odd: true}...]
 const arr = [1,2,3,5,8,9,10]
-let a = arr.map((number, arr) => ({digit: number, odd:}) )
-console.log( JSON.stringify(a))
+let a = arr.map(number => ({digit: number, odd:(number % 2 === 0)}) )
+console.log( JSON.stringify(a)) 
