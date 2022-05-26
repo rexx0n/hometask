edfdf2ac983939baceafeca50603ec94bad68133
rexx0n <alexey.mironenko46@gmail.com>
2022-05-26 20:07:48 +0300

    complete task

--- Обучение js/1. Фундаментальные основы Js/28.2 Презентация. Деструктурирующее присваивание/main.js
+++ Обучение js/1. Фундаментальные основы Js/28.2 Презентация. Деструктурирующее присваивание/main.js
@@ -13,3 +13,21 @@ function sawsda (a) {
     }
 }
 console.log(sawsda (["sasda", "asdas", "asdas", "asdasdas"]))
+
+
+// 2. Организовать функцию getInfo, которая принимает объект вида
+// { name: ...,  info: { employees: [...], partners: [ … ]  } }
+// и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+ const organisation = {  
+   name: 'Google',   
+   info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+ };
+// getInfo(organisation); → 
+// Name: Google 
+// Partners: Microsoft Facebook
+function getInfo (organisation) {
+    const {name = "Unknown", info:{employees,partners:[first,second]}} = organisation
+    return {name,  partners:`${first} ${second} `}
+}
+console.log(getInfo(organisation))
