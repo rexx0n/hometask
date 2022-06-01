a087c4ff7210b0b7a65b127cfe72c4e42b05b88a
rexx0n <alexey.mironenko46@gmail.com>
2022-06-01 22:18:19 +0300

    added task

---
+++ Обучение js/2. DOM Манипуляции элементами и события/main2.js
@@ -0,0 +1,13 @@
+// 1. Создать функцию, которая принимает два элемента. Функция проверяет, является ли первый элемент родителем для второго:
+
+// isParent(parent, child);
+// isParent(document.body.children[0], document.querySelector('mark'));
+// true так как первый див является родительским элементом для mark
+
+// isParent(document.querySelector('ul'), document.querySelector('mark'));
+ // false так ul НЕ является родительским элементом для mark
+// Функция принимает только DOM объекты.
+
+function isParent (parent1, parent2) {
+    if
+}
