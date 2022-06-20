875c40c66f991f2fa5976c5dc871698c56e7389a
rexx0n <alexey.mironenko46@gmail.com>
2022-06-20 22:04:49 +0300

    complete task

--- Обучение js/2. DOM Манипуляции элементами и события/3.1 Презентация. Основы. Манипуляция элементами и атрибуты/main.js
+++ Обучение js/2. DOM Манипуляции элементами и события/3.1 Презентация. Основы. Манипуляция элементами и атрибуты/main.js
@@ -1,42 +1,43 @@
 // Найти параграф и получить его текстовое содержимое (только текст!)
 
-let paragr = document.querySelector("p")
-console.log(paragr.textContent)
+let paragr = document.querySelector("p");
+console.log(paragr.textContent);
 
 // 2. Создать функцию, которая принимает в качестве аргумента узел DOM и возвращает информацию (в виде объекта) о типе узла, об имени узла и о количестве дочерних узлов (если детей нет - 0).
 
-function info (arg) {
-    let a = arg.nodeType
-    let b = arg.nodeName
-    let child = arg.children.length
-    return {
-        a,
-        b,
-        child
-    }
+function info(arg) {
+  let a = arg.nodeType;
+  let b = arg.nodeName;
+  let child = arg.children.length;
+  return {
+    a,
+    b,
+    child,
+  };
 }
-console.log(info(document.querySelector("div")))
+console.log(info(document.querySelector("div")));
 
 // 3. Получить массив, который состоит из текстового содержимого ссылок внутри списка: getTextFromUl(ul) ---> ["Link1", "Link2", "Link3"]
-function getTextFromUl (arg){
-    let b = document.querySelectorAll(`${arg}`).length
-    let c = []
-    for(let i = 0; i < b; i++) {
-        c += document.querySelector(`${arg}`).textContent
-    }
-    return c
+function getTextFrom(ul) {
+  const list = ul.querySelectorAll("li");
+  return [...list].map((item) => item.textContent);
 }
-console.log(getTextFromUl("ul"))
 
+const ul1 = document.querySelector("ul");
+
+console.log(getTextFrom(ul1));
 
 // Найти в коде список ul и добавить класс “list”
-let ul = document.querySelector("ul").classList.add("list")
-console.log(ul)
+let ul = document.querySelector("ul").classList.add("list");
+console.log(ul);
 
 //Найти в коде ссылку, находящуюся после списка ul, и добавить id=link
-console.log(ul.querySelector("a").id = "link")
+// console.log((ul.querySelector("a").id = "link"));
 
 // 4. В параграфе заменить все дочерние текстовые узлы на “-text-” (вложенные теги должны остаться). Конечный результат:
 // -text-<a href="#">reprehendunt</a>-text-<mark>nemore</mark>-text-
+
+const paragrh =  document.querySelector("p")
+console.log(paragr.child = "-text-")
 
 // На li через один (начиная с самого первого) установить класс “item”
