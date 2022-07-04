c89de4c35c8443b530ddc9192099412cff449a3f
rexx0n <alexey.mironenko46@gmail.com>
2022-07-04 22:07:20 +0300

    complete task

--- Обучение js/2. DOM Манипуляции элементами и события/5.2 Презентация. События/main.js
+++ Обучение js/2. DOM Манипуляции элементами и события/5.2 Презентация. События/main.js
@@ -1,6 +1,17 @@
 //1.По нажатию на кнопку "btn-msg" должен появиться алерт с тем текстом который находится в атрибуте data-text у кнопки.
-let btn = document.querySelector("#btn-msg")
- let a = btn.dataset.text
-btn.addEventListener ("click", function () {
-    alert(a)
-})
+let btn = document.querySelector("#btn-msg");
+let a = btn.dataset.text;
+btn.addEventListener("click", function () {
+  alert(a);
+});
+
+//2. При наведении указателя мыши на "btn-msg", кнопка становится красной; когда указатель мыши покидает кнопку, она становится прежнего цвета. Цвет менять можно через добавление класса.
+
+btn.addEventListener("mouseover", function () {
+  btn.classList.add("red");
+});
+btn.addEventListener("mouseout", function () {
+  btn.classList.remove("red");
+});
+//При нажатии на любой узел документа показать в элементе с id=tag имя тега нажатого элемента.
+
