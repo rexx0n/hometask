3cedf82c84793da1ae6b470098566fd0083b4982
rexx0n <alexey.mironenko46@gmail.com>
2022-08-09 14:12:44 +0300

    added task

---
+++ Обучение js/5. ООП/1. Введение в ООП. Функции конструкторы. Классы ES5/draft/main.js
@@ -0,0 +1,17 @@
+const str = new String("hello world")
+
+function Product(brand, price, discount) {
+    //1. Создается новый обьект
+    //2. На этот обьект устанавливается ссылка this
+    //3. возвращает этот обьект
+    this.brand = brand
+    this.price = price
+    this.discount = discount
+    this.getPriceWithDiscount = function () {
+        return (this.price * (100 - this.discount)) / 100
+    }
+}
+const apple = new Product("Apple", 100, 15)
+const samsung = new Product("Samsung", 200, 25)
+console.log(apple)
+console.log(samsung)
