24d5c305625b3c851911f71893a31b72c75a4977
rexx0n <alexey.mironenko46@gmail.com>
2022-08-10 01:05:00 +0300

    complete task

--- Обучение js/4. Асинхронный JavaScript. Часть №2/1. Введение в Promises. Часть 1/main.js
+++ Обучение js/4. Асинхронный JavaScript. Часть №2/1. Введение в Promises. Часть 1/main.js
@@ -1,6 +1,17 @@
-// Создать функцию, которая возвращает промис.  Функция принимает два аргумента - время, через которое промис должен выполниться, и значение, с которым промис будет выполнен. 
+// Создать функцию, которая возвращает промис.  Функция принимает два аргумента - время, через которое промис должен выполниться, и значение, с которым промис будет выполнен.
 
 // function promiseCreator(...) {...}
 // const prom = promiseCreator(500, 'Ok!');
 // prom.then(console.log);
 // // Ok!
+
+function promiseCreator(time, value) {
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(value);
+    }, time);
+  });
+  return promise;
+}
+const prom = promiseCreator(500, "Ok");
+prom.then(console.log);
