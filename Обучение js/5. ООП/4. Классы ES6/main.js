47d067b6de3a93fbf185ede9fdf442289eeadb65
rexx0n <alexey.mironenko46@gmail.com>
2022-09-03 13:41:13 +0300

    complete task

--- Обучение js/5. ООП/4. Классы ES6/main.js
+++ Обучение js/5. ООП/4. Классы ES6/main.js
@@ -14,7 +14,22 @@ class Component  {
         this.tagName = tagName
         this.node = document.createElement(tagName)
     }
+    setText (text) {
+        this.node.textContent = text
+    }
 }
 
 const comp = new Component("span")
 const compo = new Component()
+
+// Реализовать конструктор и методы в ES6 синтаксисе:
+
+// function Component(tagName) {
+//   this.tagName = tagName || 'div';
+//   this.node = document.createElement(tagName);
+// }
+
+// Component.prototype.setText = function (text) { 
+//   this.node.textContent = text;
+// };
+//Сделано выше
