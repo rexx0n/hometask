bfb2ca3f788ee29e28bd6207f8f5f4c721e493a7
rexx0n <alexey.mironenko46@gmail.com>
2022-09-03 13:55:24 +0300

    complete task

--- Обучение js/5. ООП/4. Классы ES6/main.js
+++ Обучение js/5. ООП/4. Классы ES6/main.js
@@ -9,18 +9,18 @@
 
 // const comp = new Component('span');
 
-class Component  {
-    constructor (tagName = "div"){
-        this.tagName = tagName
-        this.node = document.createElement(tagName)
-    }
-    setText (text) {
-        this.node.textContent = text
-    }
+class Component {
+  constructor(tagName = "div") {
+    this.tagName = tagName;
+    this.node = document.createElement(tagName);
+  }
+  setText(text) {
+    this.node.textContent = text;
+  }
 }
 
-const comp = new Component("span")
-const compo = new Component()
+const comp = new Component("span");
+const compo = new Component();
 
 // Реализовать конструктор и методы в ES6 синтаксисе:
 
@@ -29,7 +29,34 @@ const compo = new Component()
 //   this.node = document.createElement(tagName);
 // }
 
-// Component.prototype.setText = function (text) { 
+// Component.prototype.setText = function (text) {
 //   this.node.textContent = text;
 // };
 //Сделано выше
+
+// 3. Создать класс калькулятора который будет принимать стартовое значение и у него будут методы сложить, вычесть, умножить , разделить. Также у него должны быть геттер и сеттер для получения и установки текущего числа с которым производятся вычисления.
+class Calculator {
+  constructor(num) {
+    this._num = num;
+  }
+  addUp(nume) {
+    return (this._num = this._num + nume);
+  }
+  subtract(nume) {
+    return (this._num = this._num - nume);
+  }
+  multiply(nume) {
+    return (this._num = this._num * nume);
+  }
+  divide (nume) {
+    return (this._num = this._num / nume);
+  }
+  set num (name) {
+    this._num = name
+  }
+  get num () {
+    return this._num
+  }
+}
+
+const calc = new Calculator(2);
