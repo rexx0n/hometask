b21b8178b7dee30358e8febb3638e3aadc780c19
rexx0n <alexey.mironenko46@gmail.com>
2022-09-07 23:39:11 +0300

    work with vue

--- Обучение js/Vue/project/src/App.vue
+++ Обучение js/Vue/project/src/App.vue
@@ -1,35 +1,55 @@
 <template>
-  <div>
-    <div>
-      <button @:click="addLike">like</button>
-      <button @click="addDislike">dislike</button>
-    </div>
-  </div>
-  <div>
-    Кол-во лайков: <strong>{{ likes }}</strong>
-  </div>
-  <div>
-    Кол-во дизлайков: <strong>{{ dislikes }}</strong>
+  <div class="app">
+    <post-form/>
+    <post-list :posts="posts"/>
   </div>
 </template>
 
 <script>
+import PostForm from "./components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
 export default {
+  components: {
+    PostForm,PostList
+  },
   data() {
     return {
-      likes: 0,
-      dislikes: 5,
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript", body: "Описание поста2" },
+        { id: 3, title: "JavaScript", body: "Описание поста3" },
+        { id: 4, title: "JavaScript", body: "Описание поста4" },
+      ],
+      title: "",
+      body: "",
     };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes += 1;
+    createPost() {
+      const newPost = {
+        id:Date.now(),
+        title: this.title,
+        body: this.body
+        
+      }
+      this.posts.push(newPost)
+      this.title = ""
+      this.body = ""
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+
+
+</style>
---
+++ Обучение js/Vue/project/src/components/PostForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-bind:value="title"
+      @input="title = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      @input="body = $event.target.value"
+      v-bind:value="body"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  align-self: flex-end;
+  border: 1px solid teal;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
