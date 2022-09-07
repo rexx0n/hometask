b43504ab6b440b330718dbbc41b9c4188e8c9524
rexx0n <alexey.mironenko46@gmail.com>
2022-09-07 22:46:12 +0300

    work with vue

---
+++ Обучение js/Vue/project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <button @:click="addLike">like</button>
+      <button @click="addDislike">dislike</button>
+    </div>
+  </div>
+  <div>
+    Кол-во лайков: <strong>{{ likes }}</strong>
+  </div>
+  <div>
+    Кол-во дизлайков: <strong>{{ dislikes }}</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+
+<style></style>
