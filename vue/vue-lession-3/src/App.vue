bfca70f4a90e3256a393e4e8b8441bf69fdd8260
rexx0n <alexey.mironenko46@gmail.com>
2022-10-31 13:26:45 +0300

    work with vue

---
+++ vue/vue-lession-3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Письменные столы</h1>
+    <ul>
+      <li>
+        <product-card />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+export default {
+  name: "ProductCard",
+  components:{ProductCard},
+  data() {
+    return {
+      title: "Стол jim",
+      price: 19999,
+      imgUrl:
+        "https://www.cmp24.ru/images/prodacts/sourse/62029/62029116_stol-pismennyiy-uglovoy-jimi-la-redoute-interieurs.jpg",
+      count: 20,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-lession-3/src/components/ProductCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img :src="imgUrl" width="300px" />
+    <div>
+      <h2>{{ title }}</h2>
+      <p>{{ price }}</p>
+      <p>Достпуно для поставки{{ count }}</p>
+      <button>Добавить в корзину</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ProductCard',
+  data() {
+    return {
+      title: "Стол jim",
+      price: 19999,
+      imgUrl:'https://www.cmp24.ru/images/prodacts/sourse/62029/62029116_stol-pismennyiy-uglovoy-jimi-la-redoute-interieurs.jpg',
+      count:20
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
