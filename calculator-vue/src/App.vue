418507dcf6849c9681bf43b11781bb3c75105bf6
rexx0n <alexey.mironenko46@gmail.com>
2023-01-14 18:49:52 +0300

    calculator-vue

---
+++ calculator-vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div> 
+  <Calculator/>
+</div>
+  
+</template>
+
+<script>
+// eslint-disable-next-line
+import Calculator from '@/components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ calculator-vue/src/components/Calculator.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="calc">
+  <div>${}</div>
+    <div class="flex">
+      <button class="lightGray">AC</button>
+      <button class="lightGray">+/-</button>
+      <button class="lightGray">%</button>
+      <button class="marks">÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button class="marks">х</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button class="marks">-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button class="marks">+</button>
+      <button class="padding">0</button>
+      <button>.</button>
+      <button class="marks">=</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'сalculator',
+  
+}
+</script>
+
+<style scoped>
+.lightGray {
+  background: lightgray;
+}
+.flex{
+  margin: 0 auto;
+  display: flex;
+  max-width: 215px;
+  flex-wrap: wrap;
+  gap: 5px;
+}
+.calc {
+  display: flex;
+  margin: 0 auto;
+  background: black;
+  border-radius: 20px;
+  max-width: 245px;
+  min-height: 500px;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+.marks {
+  background: orange;
+}
+.padding {
+  padding-right: 66px;
+  padding-left: 23px;
+}
+button {
+  border-radius: 50px;
+  background: gray;
+  min-width: 50px;
+  min-height: 50px;
+  font-size: 20px;
+}
+</style>
