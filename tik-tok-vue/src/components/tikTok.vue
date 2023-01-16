a44e69f83ce7aaf44150a4efef269bd121ff11a0
rexx0n <alexey.mironenko46@gmail.com>
2023-01-16 18:20:14 +0300

    tiktok

--- tik-tok-vue/src/components/tikTok.vue
+++ tik-tok-vue/src/components/tikTok.vue
@@ -4,35 +4,67 @@
     <button class="reset">Начать заново</button>
     <div class="ss">
       <div class="tiktok">
-        <div class="tiktok__pole" id="0"></div>
-        <div class="tiktok__pole" id="1"></div>
-        <div class="tiktok__pole" id="2"></div>
-        <div class="tiktok__pole" id="3"></div>
-        <div class="tiktok__pole" id="4"></div>
-        <div class="tiktok__pole" id="5"></div>
-        <div class="tiktok__pole" id="6"></div>
-        <div class="tiktok__pole" id="7"></div>
-        <div class="tiktok__pole" id="8"></div>
+        <div @click="onClick" :class="{tiktok__pole__tik: false}" class="tiktok__pole border-right" id="0"></div>
+        <div @click="onClick" class="tiktok__pole border-right" id="1"></div>
+        <div @click="onClick" class="tiktok__pole" id="2"></div>
+        <div @click="onClick" class="tiktok__pole border-right border-top" id="3"></div>
+        <div @click="onClick" class="tiktok__pole border-right border-top tiktok__pole--tik" id="4"></div>
+        <div @click="onClick" class="tiktok__pole border-top" id="5"></div>
+        <div @click="onClick" class="tiktok__pole border-right border-top" id="6"></div>
+        <div @click="onClick" class="tiktok__pole border-right border-top" id="7"></div>
+        <div @click="onClick" class="tiktok__pole border-top" id="8"></div>
       </div>
       <div class="block">
-        <p></p>
+        <p>{{ title }}</p>
       </div>
     </div>
   </div>
 </template>
 // при клике на поле должен появлятся крестик или нолик
+// если mark === 'x' то поставить крестик и поменять на нолик если mark = '0' то поставить 0 и поменять на х
+//при клике нужно добавить класс  если 0 то добавить класс
+//
 <script>
-export default  {
+export default {
   name: 'tikTok',
-  data () {
-  return {
-    mark:'x',
-  }
+  data() {
+    return {
+      mark: 'x',
+      id: [],
+      title: 'Ходит крестик',
+      lines: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    }
   },
   methods: {
-    onClick (event) {
+    onClick(event) {
       if (this.mark === 'x') {
-        event.target.id
+        console.log(event.target)
+        event.target.classList.add("tiktok__pole__tik")
+        this.id.push(event.target.id)
+        console.log(this.id)
+        for (let i = 0; i < this.lines.length; i++) {
+          // const [a, b, c, d, e] = this.id;
+          console.log(this.lines)
+        }
+        this.mark = '0'
+        this.title = 'Ходит нолик'
+
+
+      } else {
+        console.log(event.target)
+        event.target.classList.add("tiktok__pole__tok")
+
+        this.mark = 'x'
+        this.title = 'Ходит крестик'
       }
     }
   }
@@ -40,38 +72,41 @@ export default  {
 </script>
 
 <style scoped>
-body {
-  margin: 0 auto;
-  background-color: #7029ff;;
-}
+
 .tiktok {
   display: flex;
   width: 359px;
-  height: 500px;
   justify-content: center;
-  flex-direction: column;
   flex-wrap: wrap;
 }
+
 .tiktok__pole {
-  background-color: gray;
   width: 100px;
   height: 100px;
-  margin-right: 13px;
-  margin-bottom:26px ;
 }
-.tiktok__pole--tik {
+
+.border-right {
+  border-right: 1px solid black;
+}
+
+.border-top {
+  border-top: 1px solid black;
+}
+
+.tiktok__pole__tik {
   background-image: url(../assets/x.png);
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 }
 
-.tiktok__pole--tok {
+.tiktok__pole__tok {
   background-image: url(../assets/o.png);
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 }
+
 .ss {
   display: flex;
   justify-content: center;
