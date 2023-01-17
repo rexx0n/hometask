cf417487083b4461aa50e4e575abfddc35ba29d3
rexx0n <alexey.mironenko46@gmail.com>
2023-01-17 23:03:03 +0300

    event processing

---
+++ Обучение js/11. Основы VueJs/8. Обработка событий/obrabotka-sobitia/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id='app'>
+      <button @click="onClick('123',$event)">click</button>
+      <input type="text" @keyup.esc='onKeyUp'>
+      <p>{{text}} {{someText}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      text:'',
+      someText:''
+  }
+  },
+  
+  components: {
+   
+  },
+  methods: {
+    onClick(value, e) {
+      console.log(value, e)
+    },
+    onKeyUp(e) {
+      this.text = 'ты нашел секрет'
+      this.someText = e.target.value
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
