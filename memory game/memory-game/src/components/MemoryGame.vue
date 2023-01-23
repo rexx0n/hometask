c4194e4985b1f2738a46f0aa800a8f5382afaef6
rexx0n <alexey.mironenko46@gmail.com>
2023-01-23 20:28:42 +0300

    complete reset, generatePole,  shuffle

--- memory game/memory-game/src/components/MemoryGame.vue
+++ memory game/memory-game/src/components/MemoryGame.vue
@@ -1,84 +1,115 @@
 <template>
+  <h1>{{ winner }}</h1>
+  <p>Осталось открыть {{ countClosedCard }}</p>
   <div class="flex">
-    <h1>{{winner}}</h1>
     <div @click="onClick(i,index)" class="item" v-for="(i,index) in pole" :key="index">
-      <template v-if="value === index">{{ i }} </template>
-      <template v-if="valueTwo === index">{{i}}</template>
-      <template v-if="open[index]">{{i}}</template>
+      <template v-if="value === index">{{ i }}</template>
+      <template v-if="valueTwo === index">{{ i }}</template>
+      <template v-if="open[index]">{{ i }}</template>
     </div>
-    <select>
-      <option>Easy</option>
-      <option>Medium</option>
-      <option>Hard</option>
-    </select>
-
   </div>
+  <select @change="onChange" v-model="size">
+    <option :value="6">Easy</option>
+    <option :value="18">Medium</option>
+    <option :value="36">Hard</option>
+  </select>
 </template>
 
 <script>
-//как при клике сделать чтобы показывалось цифра
-//При клике на среднию сложность добавить в массив 4 цифры, на сложную 6
+
 export default {
   name: 'MemoryGame',
   data() {
     return {
-      pole: ['1', '2', '3', '3', '2', '1'],
+      pole: ['1', '2', '3', '3', '2', '1',],
       value: '',
       valueTwo: '',
-      open:[false,false,false,false,false,false],
-      count: 0,
-      winner: '',
-
-
+      open: [false, false, false, false, false, false],
+      size: 6,
     }
   },
   methods: {
+    //перезаписать Pole на требуемое колво
+    onChange() {
+      this.reset()
+      this.pole = this.generatePole()// this.pole = this.generatepole(this.size)//в зависимости от параметра size возвращает массив[1,1,]
+       this.pole = this.shuffle()   // this.pole = this.random(this.pole)
+      // value open сбросить
+    },
+    shuffle() {
+      return this.pole.sort(() => Math.random() - 0.5);
+    },
+    generatePole() {
+      //надо чтобы 2 одинаковых числа записывались в подряд
+      for (let i = 0; i < this.size / 2; i++) {
+        this.pole.push(i)
+        this.pole.push(i)
+      }
+      console.log(this.pole)
+      return this.pole
+    },
+    reset() {
+      this.value = ''
+      this.valueTwo = ''
+      this.open = [false, false, false, false, false, false]
+      this.pole = []
+    },
     onClick(i, index) {
       if (this.value === '') {
         //1 click
         this.value = index
-        if(this.open[this.value]) {
+        if (this.open[this.value]) {
           this.value = ''
         }
       } else {
         //2 click
         this.valueTwo = index
-        if(this.open[this.valueTwo]) {
+        if (this.open[this.valueTwo]) {
           this.valueTwo = ''
         }
-        if(this.value === this.valueTwo) {
+        if (this.value === this.valueTwo) {
           this.valueTwo = ''
         }
 
         if (this.pole[this.value] === this.pole[this.valueTwo]) {
           this.open[this.value] = true
           this.open[this.valueTwo] = true
-          this.checkWinner()
           this.value = ''
           this.valueTwo = ''
         } else if (this.valueTwo !== '') {
-
-          setTimeout(()=>{
+          setTimeout(() => {
             this.value = ''
             this.valueTwo = ''
-          },1500)
+          }, 1500)
 
         }
       }
     },
+  },
+  computed: {
     checkWinner() {
       for (let i = 0; i < this.open.length; i++) {
-        if (this.open[i] !== this.open[i + 1]) {
-          return;
+        if (this.open[i] !== true) {
+          return false;
         }
-        this.count++
-        if (this.count !== 2) {
-          return
-        }
-        this.winner = "Победа"
-        console.log()
       }
+      return true
+    },
+    winner() {
+      if (this.checkWinner === true) {
+        return 'Победа'
+      }
+      return ''
+    },
+    countClosedCard() {
+      return this.open.filter((el) => {
+        if (el !== false) {
+          return false
+        }
+        return true
+      }).length
     }
+
   }
 }
 </script>
@@ -90,10 +121,11 @@ export default {
   justify-content: center;
   align-items: center;
   border: 1px solid black;
-  width: 91px;
-  height: 100px;
+  min-width: 91px;
+  min-height: 100px;
   box-shadow: inset 1px 1px 8px 14px #d2c2ac;
   background: #CCB494;
+  margin: 10px;
 }
 
 .flex {
@@ -102,7 +134,7 @@ export default {
   justify-content: center;
   flex-direction: row;
   flex-wrap: wrap;
-  width: 300px;
+  max-width: 700px;
 }
 
 </style>
