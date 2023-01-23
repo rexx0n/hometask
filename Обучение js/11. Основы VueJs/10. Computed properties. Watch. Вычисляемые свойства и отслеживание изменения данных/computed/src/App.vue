3b6db5d7c82970c3fb1050b11ed8aae69fb42776
rexx0n <alexey.mironenko46@gmail.com>
2023-01-23 15:11:29 +0300

    computed watch

---
+++ Обучение js/11. Основы VueJs/10. Computed properties. Watch. Вычисляемые свойства и отслеживание изменения данных/computed/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1> Hello {{fullName}}</h1>
+    <HelloWorld />
+  </div>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return{
+      firsName:'Aleksey',
+      lastName:''
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.firsName || 'Default'} ${this.lastName || 'User'}`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
