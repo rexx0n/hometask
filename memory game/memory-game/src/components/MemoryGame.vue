850648767e35a90c16e1f3529946ad87659830bc
rexx0n <alexey.mironenko46@gmail.com>
2023-01-24 19:53:02 +0300

    created timer, active item, button reset, made a square

--- memory game/memory-game/src/components/MemoryGame.vue
+++ memory game/memory-game/src/components/MemoryGame.vue
@@ -1,10 +1,11 @@
 <template>
-  <h1>{{ winner || 'Игра на память' }}</h1>
-  <p>Задача: найти одинаковые цифры, осталось найти {{ countClosedCard }}</p>
+  <h1>Игра на память</h1>
+  <p>Задача: найти одинаковые цифры у вас ограниченное время, осталось найти {{ countClosedCard }}</p>
+  <h1>{{ winner }}</h1>
   <div class="main">
     <div class="flex">
       <transition-group name="itemAnimation">
-        <div @click="onClick(i,index)" class="item" v-for="(i,index) in pole" :key="index">
+        <div @click="onClick(i,index)" class="item" v-for="(i,index) in pole" :key="index" :class="{'active': index === value}">
           <template v-if="value === index">{{ i }}</template>
           <template v-if="valueTwo === index">{{ i }}</template>
           <template v-if="open[index]">{{ i }}</template>
@@ -12,20 +13,20 @@
       </transition-group>
     </div>
     <div class="menu">
+      <p>Осталось время {{currentTime}}</p>
       <select @change="onChange" v-model="size">
         <option :value="6">Easy</option>
         <option :value="18">Medium</option>
         <option :value="36">Hard</option>
       </select>
-      <button>Заново</button>
+      <button @click="reset">Заново</button>
+
     </div>
   </div>
 </template>
 
 <script>
 import generatePole from "@/utils/generatePole";
-//кнопка reset
-// сделать квадратное
 // подкрашевать активные
 // таймер на экране в обратную сторону
 export default {
@@ -37,24 +38,57 @@ export default {
       value: '',
       valueTwo: '',
       open: [],
+      timer: null,
+      currentTime: 10,
+      end:false,
+      start: false,
     }
   },
   mounted() {
     this.reset()
+    this.howTime()
+  },
+  unmounted() {
+    this.stopTimer()
   },
   methods: {
     //перезаписать Pole на требуемое колво
     onChange() {
       this.reset()
     },
-
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.currentTime--
+      }, 1000)
+    },
+    stopTimer() {
+      clearTimeout(this.timer)
+    },
+    howTime () {
+      if (this.size === 6) {
+        this.currentTime = 10
+      } else if(this.size === 18) {
+        this.currentTime = 30
+      }
+      else {
+        this.currentTime = 60
+      }
+    },
     reset() {
       this.value = ''
       this.valueTwo = ''
       this.pole = generatePole(this.size)
-      this.open = Array.apply(null, {length: this.size}).map(()=> false)
+      this.open = Array.apply(null, {length: this.size}).map(() => false)
+      this.end = false
+      this.start = false
+      this.howTime()
+      this.stopTimer()
     },
     onClick(i, index) {
+      if(this.end === true) {
+        return
+      }
+      this.start = true
       if (this.value !== '' && this.valueTwo !== '') {
         this.value = ''
         this.valueTwo = ''
@@ -82,7 +116,7 @@ export default {
           this.value = ''
           this.valueTwo = ''
         } else if (this.valueTwo !== '') {
-         this.timed = setTimeout(() => {
+          this.timed = setTimeout(() => {
             this.value = ''
             this.valueTwo = ''
           }, 1000)
@@ -103,13 +137,28 @@ export default {
     winner() {
       if (this.checkWinner === true) {
         return 'Победа'
+      } else if (this.currentTime === 0) {
+        return "Ты не успел(а)"
       }
       return ''
     },
     countClosedCard() {
       return this.open.filter(el => el !== true).length
     }
-  }
+  },
+  watch: {
+    currentTime(time) {
+      if (time === 0 || this.winner === 'победа') {
+        this.stopTimer()
+        this.end = true
+      }
+    },
+    start () {
+      if (this.start === true) {
+        this.startTimer()
+      }
+    }
+  },
 }
 </script>
 
@@ -119,16 +168,19 @@ export default {
   opacity: 1;
   transition: 1s;
 }
+
 .menu {
   display: flex;
   flex-direction: column;
 
 }
+
 .main {
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .item {
   display: flex;
   justify-content: center;
@@ -140,6 +192,10 @@ export default {
   background: #CCB494;
   margin: 10px;
   font-size: 60px;
+}
+
+.active {
+  box-shadow:inset 1px 1px 8px 14px #ad997e
 }
 
 .itemAnimation-enter-active, .itemAnimation-leave-active {
@@ -159,19 +215,47 @@ export default {
   max-width: 700px;
   margin-left: 72px;
 }
-@media screen and (max-width: 320px) {
-.item {
-  min-width: 61px;
-  min-height: 61px;
-  font-size: 45px;
+
+
+@media screen and (max-width: 834px) {
+  .flex {
+    max-width: 374px;
+  }
 }
-}
+
 @media screen and (max-width: 620px) {
   .item {
     min-width: 71px;
     min-height: 71px;
     font-size: 40px;
+    margin: 5px;
+  }
+
+  .flex {
+    max-width: 291px;
   }
 }
 
+@media screen and (max-width: 418px) {
+  .item {
+    min-width: 61px;
+    min-height: 61px;
+    font-size: 45px;
+    margin: 3px;
+  }
+
+  .main {
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .menu {
+    gap: 5px;
+  }
+
+  .flex {
+    margin-left: 0;
+    max-width: 275px;
+  }
+}
 </style>
