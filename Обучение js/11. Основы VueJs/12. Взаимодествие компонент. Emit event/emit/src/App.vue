6a1a2985f11898fdd10f2e2889e8dc8b34d50eb3
rexx0n <alexey.mironenko46@gmail.com>
2023-01-29 17:18:01 +0300

    emit

---
+++ Обучение js/11. Основы VueJs/12. Взаимодествие компонент. Emit event/emit/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <HelloWorld @onChangeCounter='onChangeCounterInComponent'></HelloWorld>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    onChangeCounterInComponent(value) {
+      console.log('In App.vue, counter:', value)
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
