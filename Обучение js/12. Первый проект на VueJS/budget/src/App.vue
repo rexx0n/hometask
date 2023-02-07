7f37ed51900316841b4aa441337ed74fb277be80
rexx0n <alexey.mironenko46@gmail.com>
2023-02-07 18:48:39 +0300

    project Budget

---
+++ Обучение js/12. Первый проект на VueJS/budget/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <BudgetList :list="list"></BudgetList>
+</template>
+
+<script>
+import BudgetList from './components/BudgetList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BudgetList,
+  },
+  data() {
+    return {
+      list: {
+        1: {
+          type: 'INCOME',
+          value: 100,
+          comment:'Some comment',
+          id: 1,
+        },
+        2: {
+          type: 'OUTCOME',
+          value: -50,
+          comment:'Some out comment',
+          id: 2,
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Обучение js/12. Первый проект на VueJS/budget/src/components/BudgetList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="budget-list-wrap">
+    <ElCard :header="header">
+      <template v-if="!isEmpty"
+        ><div class="list-item" v-for="(item, prop) in list" :key="prop">
+          <span class="budget-comment">{{ item.commnet }}</span>
+          <span class="budget-value">{{ item.value }}</span>
+          <ElButton type="danger" size="mini">delete</ElButton>
+        </div></template
+      >
+      <ElAlert v-else type="info" :title="emptyTitle" :closable="false"/>
+    </ElCard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      header: "Budget List",
+      emptyTitle: 'Empty List'
+    };
+  },
+  computed: {
+    isEmpty() {
+      return !Object.keys(this.list).length
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.budget-list-wrap {
+  max-width: 500px;
+  margin: auto;
+}
+
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+</style>
