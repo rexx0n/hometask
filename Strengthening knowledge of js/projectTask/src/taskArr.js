4ca649aad8c9f10156af74c37ad7ff8f1ec3858f
rexx0n <alexey.mironenko46@gmail.com>
2023-03-20 20:44:54 +0300

    Checked 5 tasks with tests

---
+++ Strengthening knowledge of js/projectTask/src/taskArr.js
@@ -0,0 +1,70 @@
+
+// Напишите функцию, которая принимает два числа и возвращает их сумму.
+export function sum(one = 0, two = 0) {
+    return one + two
+}
+// Напишите функцию, которая принимает массив чисел и возвращает сумму всех элементов.
+export function sumArr (arr = 0) {
+    let res = 0
+    for (let i = 0; i < arr.length; i++) {
+        res += arr[i]
+    }
+    return res
+}
+// console.log(sumArr([1,2,3,4]) === 10)
+// Напишите функцию, которая принимает строку и возвращает ее длину.
+export function whatsLength(str = '') {
+    return str.length
+}
+// Напишите функцию, которая принимает массив строк и возвращает массив тех строк, которые имеют длину больше 5 символов.
+export function arrOverFive (arr) {
+    return arr.filter(word => word.length > 5)
+}
+console.log(arrOverFive(['asd','ert','Человек','фывфыафыа']))
+// Напишите функцию, которая принимает строку и возвращает ее в обратном порядке.
+export function reverse(str = '') {
+   return str.split('').reverse().join('')
+}
+// console.log(reverse('Hello') === 'olleH')
+// Напишите функцию, которая принимает массив чисел и возвращает массив, отсортированный по возрастанию.
+export function sort(arr = []) {
+    return arr.sort((a, b) => a - b );
+}
+// console.log(sortt([24,5,1,42,523,2,4,3]) == [1,2,3,4,5,24,42,523])//почему обьект 
+// Напишите функцию, которая принимает две строки и возвращает true, если они являются анаграммами, и false в противном случае.
+function annagram(strOne,strTwo) {
+    if(strOne.length !== strTwo.length) {
+        return false
+    }
+    let str1 = strOne.split('')
+    let str2 = strTwo.split('')
+    for(let i = 0; i < strOne.length; i++) {
+        if( str1.includes(str2[i]) === false) {
+            return false
+        }
+    }
+    return true
+}
+// console.log(annagram('рож','орж'))
+// Напишите функцию, которая принимает массив чисел и возвращает среднее значение.
+
+// Напишите функцию, которая принимает строку и проверяет, является ли она палиндромом (то есть читается одинаково как справа налево, так и слева направо).
+function isPalidrome(str) {
+    let a =  str.toLowerCase().split('').reverse().join('')
+    if(a === str.toLowerCase()) {
+        return true
+    }
+    return false
+}
+//   console.log(isPalidrome('Казак'))  
+// Напишите функцию, которая принимает массив строк и возвращает новый массив, содержащий только уникальные значения.
+function unical(arr) {
+    let newArr =[]
+    arr.forEach(element => {
+        if( !newArr.includes(element)) {
+            newArr.push(element)
+        }
+    });
+    return newArr
+}
+// console.log(unical(['еж','еж','asd','asr']))
--- Strengthening knowledge of js/projectTask/test/taskObjec.test.js
+++ Strengthening knowledge of js/projectTask/test/taskObjec.test.js
@@ -1,5 +1,6 @@
 import { expect, test, describe } from 'vitest';
-import { objectLength } from '../src/taskObject';
+import { objectLength} from '../src/taskObject';
+import { sum, sumArr, whatsLength, arrOverFive, reverse, sort } from '../src/taskArr';
 
 describe('тестирование функции objectLength', () => {
     test('non empty object', () => {
@@ -14,5 +15,69 @@ describe('тестирование функции objectLength', () => {
         
     })
 })
+describe('тестирование функции sum', () => {
+    test('work function', ()=> {
+        expect(sum(2, 2)).toBe(4)
+    })
+    test('is empty',()=> {
+        expect(sum()).toBe(0)
+    })
+    test('negative values',()=> {
+        expect(sum(-2, -2)).toBe(-4)
+    })
+})
+describe('тестирование функции sumArr', () => {
+    test('work function', ()=> {
+        expect(sumArr([1,2,3,4])).toBe(10)
+    })
+    test('negative values', ()=> {
+        expect(sumArr([2,3,-5])).toBe(0)
+    })
+    test('is empty', ()=> {
+        expect(sumArr([])).toBe(0)
+    })
+    test('nothing', ()=> {
+        expect(sumArr()).toBe(0)
+    })
+})
+describe('тестирование функции whatsLength', ()=> {
+    test('work function', ()=> {
+        expect(whatsLength('код')).toBe(3)
+    })
+    test('is empty', ()=> {
+        expect(whatsLength('')).toBe(0)
+    })
+    test('is nothing', ()=> {
+        expect(whatsLength()).toBe(0)
+    })
+})
+// describe('Тестирование функции arrOverFive', ()=> {
+//     test('work func', ()=> {
+//         expect(arrOverFive(['asd','ert','Человек','фывфыафыа'])).toBe(['Человек','фывфывфыф'])
+//     })
+// })
+describe('тестирование функции reverse', ()=> {
+    test('work func', ()=> {
+        expect(reverse('Казак')).toBe('казаК')
+    })
+    test('is empty', ()=> {
+        expect(reverse('')).toBe('')
+    })
+    test('is nothing', ()=> {
+        expect(reverse()).toBe('')
+    })
+})
+describe('тестирование функции sort', ()=> {
+    test('work func', ()=> {
+        expect(sort([24,5,1,42,523,2,4,3])).toStrictEqual([1,2,3,4,5,24,42,523])
+    })
+    test('is empty', ()=> {
+        expect(sort([])).toStrictEqual([])
+    })
+    test('is nothing', ()=> {
+        expect(sort()).toStrictEqual([])
+    })
+})
+
 
 
