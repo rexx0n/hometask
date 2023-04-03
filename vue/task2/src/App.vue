cf4a025d4f0ed4fa67c7ae0c4e3279a51475482a
rexx0n <alexey.mironenko46@gmail.com>
2023-04-03 23:55:37 +0300

    did the design and a little validation

--- vue/task2/src/App.vue
+++ vue/task2/src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <myRegistration ></myRegistration>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myRegistration from './components/myRegistration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myRegistration
   }
 }
 </script>
 
 <style>
+body {
+  background: url("https://pcvector.net/uploads/posts/2018-08/1533326959_low-poly-background-generator-min.png");
+  background-repeat:no-repeat;
+  background-size: cover;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ vue/task2/src/components/myRegistration.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- Создание формы регистрации с валидацией полей: Создайте форму регистрации,
+  используя компоненты Vue.js, и добавьте валидацию полей, например, проверку на
+  пустые значения, соответствие формата e-mail и пароля. -->
+  <div>
+    <form @submit.prevent action="">
+      <h2>
+        Регистрация
+      </h2>
+      <p>{{message}}</p>
+      <div class="inputs">
+        <input v-model="emailValue" type="email" placeholder="email">
+        <input v-model="passwordValue" type="password" placeholder="password">
+      </div>
+      <button @click="reg">Зарегистрироваться</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      emailValue:'',
+      passwordValue:'',
+      message:'',
+    }
+  },
+  methods: {
+    reg() {
+      if (this.emailValue === '' || this.passwordValue === '') {
+        this.message = 'Заполните поля!'
+        setTimeout(()=> {
+          this.message = ''
+        }, 3500)
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  color: white;
+  font-size: 26px;
+}
+p {
+  color: #ff0000ad;
+  font-size: 20px;
+}
+button {
+  font-size: 20px;
+  padding: 10px;
+  color: white;
+  border: 1px solid white;
+  background: rgba(255, 255, 255, 0.2);
+  backdrop-filter:blur(8px);
+  border-radius: 7px;
+}
+form {
+  margin: auto;
+  max-width: 400px;
+  padding: 30px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius:20px ;
+  border: 1px solid white;
+  backdrop-filter:blur(8px);
+}
+input {
+  max-width: 300px;
+  width: 100%;
+  padding: 10px;
+  border-radius: 7px;
+  border: none;
+}
+.inputs {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+</style>
