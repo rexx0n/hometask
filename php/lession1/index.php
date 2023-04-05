1101a47168efd0825361b42a62ed18a9d3b2e10b
rexx0n <alexey.mironenko46@gmail.com>
2023-04-05 14:58:06 +0300

    a little php

---
+++ php/lession1/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php</title>
+</head>
+<body>
+
+
+<?php
+    const MY_AGE = 23;//const
+echo MY_AGE;
+    echo '<h1>Hello</h1>';
+    echo 'World';
+    echo "\"";
+    $num = 5;
+    $number = 45;
+    echo 'Сумма: '.$num.$number;
+    $a = 0.5;
+    $b = '0.5';
+    echo  $a + floatval($b);
+
+?>
+</body>
+</html>
+
+
