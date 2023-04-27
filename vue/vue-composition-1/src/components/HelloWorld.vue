7e5407f5cb46bb5cf37cce29dfa9d9b8834a54fa
rexx0n <alexey.mironenko46@gmail.com>
2023-04-27 20:26:16 +0300

    vue composition

---
+++ vue/vue-composition-1/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <h1 @click="plus">{{state.count}}</h1>
+      <h1 @click="minus">{{count2}}</h1>
+      <h1>{{счетчик}}</h1>
+  </div>
+</template>
+
+<script setup>
+import {computed, reactive, ref} from "vue";
+
+     const state= reactive({count:0})
+      function plus() {
+         state.count++
+      }
+      function minus() {
+         count2.value--
+      }
+      const count2 = ref(10)
+      const счетчик = ref(20)
+      const log = computed(()=> {
+          return console.log('asdasda')
+      })
+
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
