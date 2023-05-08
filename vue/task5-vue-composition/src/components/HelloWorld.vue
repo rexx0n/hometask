7452f422d43d2eca159839e4214c6bfe44638c16
rexx0n <alexey.mironenko46@gmail.com>
2023-05-08 22:16:43 +0300

    adaptive carousel on js and started makin task5 on vue composition api

---
+++ vue/task5-vue-composition/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <!--  Создание слайдера изображений:
+Создайте компонент для отображения слайдера изображений, используя Vue.js,
+и добавьте возможность переключения между изображениями с помощью кнопок или автоматически через определенный интервал времени.-->
+  <div>
+      <div class="carousel">
+          <button>
+              ←
+          </button>
+          <div>
+              <div>
+                  <img src="" alt="">
+                  <img src="" alt="">
+                  <img src="" alt="">
+                  <img src="" alt="">
+                  <img src="" alt="">
+              </div>
+          </div>
+          <button>
+
+          </button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
