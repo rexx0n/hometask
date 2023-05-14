a18b96eb19706cdc923733079cd71a7740241e67
rexx0n <alexey.mironenko46@gmail.com>
2023-05-14 22:37:05 +0300

    slot vue composition

---
+++ vue/vue-composition-4/src/components/MyButton.vue
@@ -0,0 +1,17 @@
+<template>
+      <div>
+          <button class="btn">
+              <slot></slot>
+          </button>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "MyButton"
+}
+</script>
+
+<style scoped>
+
+</style>
