91fa957b0a6fc8a591500c3f9801e4045d606813
rexx0n <alexey.mironenko46@gmail.com>
2023-06-08 15:27:25 +0300

    Promise

--- Strengthening knowledge of js/promise/main.js
+++ Strengthening knowledge of js/promise/main.js
@@ -11,3 +11,37 @@ let b = new Promise(function(resolve, rejects) {
 b.then(function(){
     console.log(a);
 })
+
+// setTimeout(()=> {
+//     console.log('preparing');
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+
+// }, 2000)
+
+const p = new Promise (function(resolve, rejects){
+    setTimeout(()=> {
+        console.log('preparind data');
+         const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    }, 2000)
+})
+p.then((data)=> {
+    console.log('promise resolved', data);
+    const p2 = new Promise((resolve, rejects)=> {
+        setTimeout(()=> {
+            data.modufied = true
+            resolve(data)
+        })
+    })
+    p2.then(clientData => [
+        
+    ])
+})
