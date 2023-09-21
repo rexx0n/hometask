68164d4cc65151808a5059d03a4ca0cbd5fc2b4a
rexx0n <alexey.mironenko46@gmail.com>
2023-09-21 15:52:16 +0300

    complete task

--- Strengthening knowledge of js/tasks/Working with arr/main.js
+++ Strengthening knowledge of js/tasks/Working with arr/main.js
@@ -6,3 +6,21 @@ function biggest(arr) {
 }
 biggest(arr)
 // Реализуйте функцию, которая удаляет все дубликаты из массива.
+let arr2 = [1,2,2,3,3,4,5,6,7,7]
+function filter(arr) {
+   return  arr.filter((item, index, self) =>
+       self.indexOf(item)  === index
+   )
+}
+
+console.log(filter(arr2))
+//Создайте функцию, которая инвертирует порядок элементов в массиве.
+function invert(arr) {
+    let newArr = []
+    for(let i =arr.length -1; i > -1; i--) {
+       newArr.push(arr[i])
+    }
+    return newArr
+}
+
+console.log(invert([1,2,3,4]))
