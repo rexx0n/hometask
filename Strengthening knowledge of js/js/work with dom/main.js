73f54049290d9e05f92bf0ce14ccfbd0fcbd37e8
rexx0n <alexey.mironenko46@gmail.com>
2023-12-07 16:24:12 +0300

    complete task

--- Strengthening knowledge of js/js/work with dom/main.js
+++ Strengthening knowledge of js/js/work with dom/main.js
@@ -9,3 +9,15 @@ body.addEventListener(('click'),()=> {
         }
     body.style.background = colors[count]
 })
+//Реализуйте функцию, которая добавляет новый элемент в DOM с использованием чистого JavaScript.
+const button = document.querySelector('button')
+let h1
+let textH1
+function addElement() {
+    h1 = document.createElement('h1')
+    textH1 = document.createTextNode('Hello this is h1')
+    h1.appendChild(textH1)
+    document.body.append(h1)
+}
+
+button.addEventListener(('click'), addElement)
