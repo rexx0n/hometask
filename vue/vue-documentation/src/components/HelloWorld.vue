bdb92d3a697d0c3e85ce40c7265a3593cd965271
rexx0n <alexey.mironenko46@gmail.com>
2024-01-23 16:58:56 +0300

    wrote from the documentation

---
+++ vue/vue-documentation/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ msg }}</h1>
+    <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span>
+</template>
+
+<script setup>
+import {computed, reactive, ref} from 'vue'
+
+defineProps({
+    msg: String,
+})
+const author = reactive({
+    name: 'John Doe',
+    books: [
+        'Vue 2 - Advanced Guide',
+        'Vue 3 - Basic Guide',
+        'Vue 4 - The Mystery'
+    ]
+})
+
+// a computed ref
+const publishedBooksMessage = computed(() => {
+    return author.books.length > 0 ? 'Yes' : 'No'
+})
+
+console.log(publishedBooksMessage.value)
+
+</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
